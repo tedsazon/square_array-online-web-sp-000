a0127a50c402f76df5c1b79978e450a9fdb0325e
Ted Sazon <tedsazon@gmail.com>
2018-12-14 17:39:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   new_array = []
-  array.each do |x|
-    new_array << x ** 2
-    end
-    new_array
+  array.each do |number|
+    new_array << number ** 2
+  end
+  new_array
+
 end
